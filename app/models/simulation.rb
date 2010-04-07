b8b29d31aa47d3dcc055b61214588bf728fab4c5
Laurent Drouet <lolow@lolow-laptop.(none)>
2010-04-07 10:07:09 +0200

    simulations - files

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -2,12 +2,30 @@ class Simulation < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
+  before_destroy :delete_results
+
+  EXT = %w{mod dat log out csv}
+
   def store_results(solver)
     return unless solver.finished? && solver.optimal?
-    ext = %w{mod dat log out csv}
-    path = File.join(RAILS_ROOT,'public','files','sim',self.id.to_s)
-    FileUtils.mkdir_p(path)
-    ext.each { |x| FileUtils.cp(solver.file(x),File.join(path,"sim.#{x}"))  }
+    FileUtils.mkdir_p(results_path)
+    EXT.each do |x|
+      FileUtils.cp(solver.file(x),File.join(results_path,"sim.#{x}"))
+    end
+  end
+
+  private
+
+  def results_path
+    File.join(RAILS_ROOT,'public','files','sim',self.id.to_s)
+  end
+
+  def delete_results
+    EXT.each do |x|
+      f = File.join(results_path,"sim.#{x}")
+      File.delete(f) if File.exists?(f)
+    end
+    FileUtils.rmdir(results_path)
   end
 
 end
