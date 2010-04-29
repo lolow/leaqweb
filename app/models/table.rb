ea975b5d4ef9ae3af39e5adcf9a6a9c02eacccb0
Laurent Drouet <lolow@lolow-laptop.(none)>
2010-04-29 19:01:30 +0200

    table predefined edition

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,27 +2,39 @@ class Table < ActiveRecord::Base
 
   AGGREGATES = %w{SUM MEAN}
   VARIABLES = %w{VAR_OBJINV VAR_OBJFIX VAR_OBJVAR VAR_OBJSAL CAPACITY ACTIVITY VAR_IMP VAR_EXP VAR_COM VAR_ICAP}
+  INDEX = { "T" => "Time period",
+            "S" => "Time slice",
+            "L" => "Location",
+            "P" => "Processes",
+            "C" => "Commodities" }.freeze
 
-  def detailed_filter
+  def digest_filter
     filters.split('&').collect { |term|
       term = term.split(" ",3)
-      p term
       condition = Hash.new
-      condition[:not] = true if term[0][0]=="!"
-      condition[:var] = term[0].gsub('!','')
+      if term.first.first=="!"
+        condition[:not] = "not "
+      else
+        condition[:not] = ""
+      end
+      condition[:variable] = INDEX[term[0].gsub('!','').strip]
       break unless term[1].index("%in%")
       if term[2].strip.index("grep('^")==0
-        condition[:func] = :start_with
+        condition[:func] = "start with"
         condition[:arg] = term[2].strip.split("'")[1][1..-1]
       elsif term[2].strip.index("grep('")==0
-        condition[:func] = :contains
+        condition[:func] = "contain"
         condition[:arg] = term[2].strip.split("'")[1]
       elsif term[2].strip.index("c('")==0
-        condition[:func] = :list
+        condition[:func] = "belong to"
         condition[:arg] = term[2].scan(/'\w+'/).collect{|w|w[1..-2]}.join(",")
       end
       condition
     }
+  end
+
+  def unused
+    INDEX.keys - rows.split(',') - columns.split(',')
   end
 
 end
