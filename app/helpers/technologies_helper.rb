da4dcca4468b60555b8a58be63ae155eb6f755c8
Laurent Drouet <ldrouet@gmail.com>
2010-05-10 22:56:09 +0200

    design

--- app/helpers/technologies_helper.rb
+++ app/helpers/technologies_helper.rb
@@ -7,4 +7,11 @@ module TechnologiesHelper
   def commodity_list(flow)
     "(" + flow.commodities.collect { |c| h(c.name) }.join(", ") + ")"
   end
+  def short_flow(flow)
+    s = "#{flow.id}"
+    s += ": (" + truncate(h(flow.commodities.first.name), :omission => "&hellip;", :length => 10) if flow.commodities.size > 0
+    s += ",&hellip;" if flow.commodities.size > 1
+    s += ")" if flow.commodities.size > 0
+    s
+  end
 end
