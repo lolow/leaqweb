370fe26c87cc532e1b4164218a16cfa59e9d2bce
lolow <ldrouet@gmail.com>
2010-06-02 17:22:42 +0200

    edition demand drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -45,5 +45,60 @@ class DriversController < ApplicationController
       end
     end
   end
+  
+ # DELETE /drivers/1
+  def destroy
+    @demand_driver = DemandDriver.find(params[:id])
+    @demand_driver.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(demand_drivers_url) }
+    end
+  end
+
+  # PUT /drivers/1
+  def update
+    @demand_driver = DemandDriver.find(params[:id])
+    # jeditable fields
+    if params[:field]
+      f = params[:field].split("-")
+      record = ParameterValue.find(f[1].to_i)
+      attributes = {f[2]=>params[:value]}
+      if record.update_attributes(attributes)
+        value = params[:value]
+      else
+        value = ''
+      end
+      respond_to do |format|
+        format.js { render :json => value }
+      end
+      return
+    end
+    # action on parameter_value
+    case params[:do]
+    when "update"
+      @demand_driver.update_attributes(params[:demand_driver])
+      respond_to do |format|
+        if @demand_driver.update_attributes(params[:demand_driver])
+          flash[:notice] = 'Demand driver was successfully updated.'
+          format.html { redirect_to(edit_demand_driver_path(@technology)) }
+        else
+          format.html { render :action => "edit" }
+        end
+      end
+      return
+    when "delete_pv"
+      ids = @demand_driver.parameter_values.map(&:id).select{|i|params["cb#{i}"]}
+      ParameterValue.destroy(ids)
+    when "add_pv"
+      att = params[:pv]
+      att[:parameter] = @demand_driver
+      pv = ParameterValue.new(att)
+      flash[:notice] = 'Demand driver value was successfully added.' if pv.save
+    end if params[:do]
+    respond_to do |format|
+        format.html { redirect_to(edit_demand_driver_path(@demand_driver)) }
+    end
+  end
 
 end
