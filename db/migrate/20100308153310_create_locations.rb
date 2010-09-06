fc9885533ca1c381283079660f8ced7337673d46
Laurent Drouet <ldrouet@gmail.com>
2010-09-06 10:05:06 +0200

    location migration

--- db/migrate/20100308153310_create_locations.rb
+++ db/migrate/20100308153310_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration
   def self.up
     create_table :locations do |t|
       t.string :name
-      t.description :text
+      t.text :description
       t.timestamps
     end
     add_index  :locations, :name, :unique => true
