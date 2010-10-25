4768ef913f9d430dcab6858a504e18bc0c5fb227
Laurent Drouet <ldrouet@gmail.com>
2010-10-25 18:11:19 +0200

    add links in solver log

--- app/helpers/solver_helper.rb
+++ app/helpers/solver_helper.rb
@@ -1,2 +1,8 @@
 module SolverHelper
+  def log_with_link(log)
+    log.gsub!(/t\d+/){|s| raw link_to(s, :controller=>"technologies",:action=>"edit",:id=>s[1,s.size-1]) }
+    log.gsub!(/c\d+/){|s| raw link_to(s, :controller=>"commodities",:action=>"edit",:id=>s[1,s.size-1]) }
+    log.gsub!(/f\d+/){|s| raw link_to(s, :controller=>"technologies",:action=>"edit",:id=>Flow.find(s[1,s.size-1]).technology.id) }
+    log
+  end
 end
