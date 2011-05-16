edce66acd56aa909ec8f2cf204afe85565301481
Laurent Drouet <ldrouet@gmail.com>
2011-05-16 23:56:30 +0200

    some changes

--- db/migrate/20100427142157_create_stored_queries.rb
+++ db/migrate/20100427142157_create_stored_queries.rb
@@ -1,8 +1,8 @@
 class CreateStoredQueries < ActiveRecord::Migration
   def self.up
     create_table :stored_queries do |t|
-      t.string :name, :aggregate, :variable, :rows, :columns
-      t.text :filters
+      t.string :name, :aggregate, :variable, :rows, :columns, :display
+      t.text :filters, :options
       t.timestamps
     end
   end
